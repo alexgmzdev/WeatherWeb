287bb51d56100b1edbf5ca3b3b1e096c09da47f6
alexgmz.dev <contacto.alejandrogimenez@gmail.com>
2023-05-24 21:12:35 +0200

    Cambios v2 HTML, CSS, JS

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-var url="https://raw.githubusercontent.com/alexgmzdev/urlapiweather/main/openweaterapi.json";
+function crearElementos () {
+
+    var url="https://raw.githubusercontent.com/alexgmzdev/urlapiweather/main/openweaterapi.json";
+
+    fetch(url) 
+        .then(response => response.text())
+        .then(respuesta => mostrarTiempo(respuesta))
+}
+
+function mostrarTiempo(respuesta){
+    var infoClima= JSON.parse(respuesta);
+
+    var ciudad = document.getElementById("#ciudad").value = infoClima.name;
+    
+
+}
